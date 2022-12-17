20b953cd0ffa3b73c4242853f800d77eac3b4e9a
pokogas <61144714+pokogas@users.noreply.github.com>
2022-12-17 23:25:36 +0900

    frontend customerConfirmationMiddleware.js 修正

--- frontend/service/middleware/customer/customerConfirmationMiddleware.js
+++ frontend/service/middleware/customer/customerConfirmationMiddleware.js
@@ -1,6 +1,6 @@
 export default async function ({ params, $axios, error }) {
-  await $axios.$get(`/customer/detail?customer_id=${params.id}`).then(function (res) {
-  }).catch(function () {
+  await $axios.$get(`api/customer/detail?customer_id=${params.id}`).then(function (res) {
+  }).catch(function (res) {
     error({
       statusCode: 404,
       message: 'mes'
